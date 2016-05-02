9b44c05478d1f1ae8849bad9e742ba2d14bf3f8e
Sebastian Farago <sebastianfarago@Sebastians-MacBook-Pro.local>
2016-05-02 12:39:53 +0200

    feat: added autoform, made basic files & basic routing

---
+++ client/posts.js
@@ -0,0 +1,13 @@
+Posts = new Mongo.Collection('posts');
+Posts.attachSchema(new SimpleSchema({
+	title: {
+	    type: String,
+	    label: "Title",
+	    max: 200
+	},
+	content: {
+	    type: String,
+	    label: "Content",
+	    max: 1000
+	}
+}));
