a56231c189366e562491deb64b8972be23346d37
Sebastian Farago <sebastianfarago@Sebastians-MacBook-Pro.local>
2016-05-03 16:23:48 +0200

    feat: added styles with materialize

--- client/singlePost.js
+++ client/singlePost.js
@@ -1,8 +1,8 @@
 Template.singlePost.events({
 	'click .glyphicon-thumbs-up': function () {
-		if(this.user!=Meteor.userId()){
+		if(this.liker!=Meteor.userId()){
 			Posts.update(this._id,{$inc: {likes: 1}});
-			Posts.update(this._id,{$set: {user: Meteor.userId()}});
+			Posts.update(this._id,{$set: {liker: Meteor.userId()}});
 		}
 	}
 });
