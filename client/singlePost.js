9867c84e7e98c01a6920260787a5dcb14515bcec
Sebastian Farago <sebastianfarago@Sebastians-MacBook-Pro.local>
2016-05-03 12:52:40 +0200

    feat: added limitation to likes

--- client/singlePost.js
+++ client/singlePost.js
@@ -1,5 +1,8 @@
 Template.singlePost.events({
 	'click .glyphicon-thumbs-up': function () {
-		Posts.update(this._id,{$inc: {likes: 1}})	
+		if(this.user!=Meteor.userId()){
+			Posts.update(this._id,{$inc: {likes: 1}});
+			Posts.update(this._id,{$set: {user: Meteor.userId()}});
+		}
 	}
 });
